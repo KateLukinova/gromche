4c7f83bb06b293b675adb4c2650ed284c35715ca
Екатерина Лукинова <katyushta@rambler.ru>
2020-07-27 00:10:57 +0300

    side

--- js/scripts.js
+++ js/scripts.js
@@ -26,5 +26,45 @@ $( document ).ready(function() {
         dots: true
     });
 
+    //tabs
+    $(".progress-item").click(function() {
+        $(".progress-item").removeClass("active").eq($(this).index()).addClass("active");
+        $(".tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
+
+    //progress bar
+
+    var bar = new ProgressBar.Circle(circleProgress, {
+        color: '#73738D',
+        // This has to be the same size as the maximum width to
+        // prevent clipping
+        strokeWidth: 35,
+        trailWidth: 1,
+        easing: 'easeInOut',
+        duration: 1400,
+        text: {
+            autoStyleContainer: false
+        },
+        from: { color: '#6EB2C1', width: 1 },
+        to: { color: '#6EB2C1', width: 35 },
+        // Set default step function for all animate calls
+        step: function(state, circle) {
+            circle.path.setAttribute('stroke', state.color);
+            //circle.path.setAttribute('stroke-width', state.width);
+
+            var value = Math.round(circle.value() * 100);
+            if (value === 0) {
+                circle.setText('');
+            } else {
+                circle.setText(value + '%');
+            }
+
+        }
+    });
+
+    bar.animate(0.25);  // Number from 0.0 to 1.0
+
+
+
 
 });
